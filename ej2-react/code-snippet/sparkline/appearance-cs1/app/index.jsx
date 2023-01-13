8d8b4810ee56587c854b2db03b6b74b5b1d55a5f
Build Automaion <buildautomation@syncfusion.com>
2023-01-13 11:02:00 +0530

    Integrated latest changes at 01-13-2023 11:00:15 AM

--- ej2-react/code-snippet/sparkline/appearance-cs1/app/index.jsx
+++ ej2-react/code-snippet/sparkline/appearance-cs1/app/index.jsx
@@ -3,9 +3,7 @@ import * as ReactDOM from "react-dom";
 import { SparklineComponent } from '@syncfusion/ej2-react-charts';
 export class App extends React.Component {
     render() {
-        return (<SparklineComponent id='sparkline' height='200px' width='350px' containerArea={{
-                border: { color: '#033e96', width: 2 }
-            }} type='Area' fill='#b2cfff' dataSource={[3, 6, 4, 1, 3, 2, 5]}>
+        return (<SparklineComponent id='sparkline' height='200px' width='350px' containerArea={{ border: { color: '#033e96', width: 2 } }} type='Area' fill='#b2cfff' dataSource={[3, 6, 4, 1, 3, 2, 5]}>
         </SparklineComponent>);
     }
 }
