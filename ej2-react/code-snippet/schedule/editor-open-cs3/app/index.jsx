a6b7897180e608026fc4ce6b83fe6257bb2f5c5c
Build Automaion <buildautomation@syncfusion.com>
2023-03-02 21:24:45 +0530

    Integrated latest changes at 03-02-2023 6:11:54 PM

--- ej2-react/code-snippet/schedule/editor-open-cs3/app/index.jsx
+++ ej2-react/code-snippet/schedule/editor-open-cs3/app/index.jsx
@@ -5,31 +5,33 @@ import { Day, Week, WorkWeek, Month, ScheduleComponent, ViewsDirective, ViewDire
 import { extend } from '@syncfusion/ej2-base';
 import { scheduleData } from './datasource';
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
+
 function App() {
-    let scheduleObj;
-    const data = extend([], scheduleData, null, true);
-    function onClick() {
-        let btnElement = document.querySelector('#btn1');
-        let btnElement2 = document.querySelector('#btn2');
-        btnElement.innerText = 'Change the work hours';
-        btnElement2.style.display = 'none';
-        let dates = [new Date(2018, 1, 15), new Date(2018, 1, 17)];
-        scheduleObj.setWorkHours(dates, '11:00', '20:00');
+  let scheduleObj;
+  const data = extend([], scheduleData, undefined, true);
+  function onClick() {
+    if (scheduleObj) {
+      let dates = [new Date(2018, 1, 15), new Date(2018, 1, 17)];
+      scheduleObj.setWorkHours(dates, '11:00', '20:00');
     }
-    return (<div>
-    <ButtonComponent id='btn1' title='Click to open Editor' onClick={onClick}>Click to open Editor</ButtonComponent>
-    <ButtonComponent id='btn2' title='Click to open Event Editor' onClick={onClick}>Click to open Event Editor</ButtonComponent>
-    <ScheduleComponent ref={t => scheduleObj = t} height='550px' selectedDate={new Date(2018, 1, 15)} workHours={{ highlight: true, start: '09:00', end: '11:00' }} eventSettings={{ dataSource: data }}>
-      <ViewsDirective>
-        <ViewDirective option='Day'/>
-        <ViewDirective option='Week'/>
-        <ViewDirective option='WorkWeek'/>
-        <ViewDirective option='Month'/>
-      </ViewsDirective>
-      <Inject services={[Day, Week, WorkWeek, Month]}/>
-    </ScheduleComponent>
-  </div>);
-}
-;
+  }
+
+  return (
+    <div>
+      <ButtonComponent title='Set work hours' onClick={onClick}>Click to set work hours</ButtonComponent>
+      <ScheduleComponent ref={t => scheduleObj = t} height='550px' selectedDate={new Date(2018, 1, 15)}
+        workHours={{ highlight: true, start: '09:00', end: '11:00' }}
+        eventSettings={{ dataSource: data }}>
+        <ViewsDirective>
+          <ViewDirective option='Day' />
+          <ViewDirective option='Week' />
+          <ViewDirective option='WorkWeek' />
+          <ViewDirective option='Month' />
+        </ViewsDirective>
+        <Inject services={[Day, Week, WorkWeek, Month]} />
+      </ScheduleComponent>
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById('schedule'));
 root.render(<App />);
--- ej2-react/code-snippet/schedule/editor-open-cs3/app/index.tsx
+++ ej2-react/code-snippet/schedule/editor-open-cs3/app/index.tsx
@@ -1,47 +1,36 @@
 
-
-
-
 import * as ReactDOM from 'react-dom';
 import * as React from 'react';
-import {
-  Day, Week, WorkWeek, Month, ScheduleComponent, ResourcesDirective,
-  ResourceDirective, ViewsDirective, ViewDirective,
-  Inject
-} from '@syncfusion/ej2-react-schedule';
+import { Day, Week, WorkWeek, Month, ScheduleComponent, ViewsDirective, ViewDirective, Inject } from '@syncfusion/ej2-react-schedule';
 import { extend } from '@syncfusion/ej2-base';
 import { scheduleData } from './datasource';
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
 
 function App() {
-  let scheduleObj: ScheduleComponent;
-  const data: Object[] = extend([], scheduleData, null, true) as Object[];
+  let scheduleObj: ScheduleComponent | null;
+  const data: Object[] = extend([], scheduleData, undefined, true) as Object[];
   function onClick(): void {
-    let btnElement: HTMLElement = document.querySelector('#btn1') as HTMLElement;
-    let btnElement2: HTMLElement = document.querySelector('#btn2') as HTMLElement;
-    btnElement.innerText = 'Change the work hours';
-    btnElement2.style.display = 'none';
-    let dates: Date[] = [new Date(2018, 1, 15), new Date(2018, 1, 17)];
-    scheduleObj.setWorkHours(dates, '11:00', '20:00');
+    if (scheduleObj) {
+      let dates: Date[] = [new Date(2018, 1, 15), new Date(2018, 1, 17)];
+      scheduleObj.setWorkHours(dates, '11:00', '20:00');
+    }
   }
-  return (<div>
-    <ButtonComponent id='btn1' title='Click to open Editor' onClick={onClick}>Click to open Editor</ButtonComponent>
-    <ButtonComponent id='btn2' title='Click to open Event Editor' onClick={onClick}>Click to open Event Editor</ButtonComponent>
-    <ScheduleComponent ref={t => scheduleObj = t} height='550px' selectedDate={new Date(2018, 1, 15)}
-      workHours={{ highlight: true, start: '09:00', end: '11:00' }}
-      eventSettings={{ dataSource: data }}>
-      <ViewsDirective>
-        <ViewDirective option='Day' />
-        <ViewDirective option='Week' />
-        <ViewDirective option='WorkWeek' />
-        <ViewDirective option='Month' />
-      </ViewsDirective>
-      <Inject services={[Day, Week, WorkWeek, Month]} />
-    </ScheduleComponent>
-  </div>)
+  return (
+    <div>
+      <ButtonComponent title='Set work hours' onClick={onClick}>Click to set work hours</ButtonComponent>
+      <ScheduleComponent ref={t => scheduleObj = t} height='550px' selectedDate={new Date(2018, 1, 15)}
+        workHours={{ highlight: true, start: '09:00', end: '11:00' }}
+        eventSettings={{ dataSource: data }}>
+        <ViewsDirective>
+          <ViewDirective option='Day' />
+          <ViewDirective option='Week' />
+          <ViewDirective option='WorkWeek' />
+          <ViewDirective option='Month' />
+        </ViewsDirective>
+        <Inject services={[Day, Week, WorkWeek, Month]} />
+      </ScheduleComponent>
+    </div>
+  );
 };
 const root = ReactDOM.createRoot(document.getElementById('schedule'));
 root.render(<App />);
-
-
-
