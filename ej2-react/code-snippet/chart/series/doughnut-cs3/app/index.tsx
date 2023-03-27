c6da298a09af72a6595947ba5fd31a4efc2cc2ba
Build Automaion <buildautomation@syncfusion.com>
2023-03-27 17:39:20 +0530

    Integrated latest changes at 03-27-2023 3:04:06 PM

--- ej2-react/code-snippet/chart/series/doughnut-cs3/app/index.tsx
+++ ej2-react/code-snippet/chart/series/doughnut-cs3/app/index.tsx
@@ -7,7 +7,7 @@ import { centerData } from 'datasource.ts';
 
 function App() {
 
-    return <AccumulationChartComponent id='charts' centerLabel={{text : 'Mobile<br>Browsers<br>Statistics', hoverTextFormat: '${point.x} <br> Browser Share <br> ${point.y}%'}}>
+    return <AccumulationChartComponent id='charts' centerLabel={{text : 'Mobile<br>Browsers<br>Statistics', hoverTextFormat: '${point.x}Browser Share${point.y}%'}}>
       <AccumulationSeriesCollectionDirective>
         <AccumulationSeriesDirective dataSource={centerData} xName='x' yName='y' innerRadius='65%'>
         </AccumulationSeriesDirective>
