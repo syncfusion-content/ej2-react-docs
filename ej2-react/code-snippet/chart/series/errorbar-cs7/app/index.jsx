6e0e4c85dd60b844222c2a00d3037c53bc961536
Build Automaion <buildautomation@syncfusion.com>
2023-04-25 13:25:27 +0530

    Integrated latest changes at 04-25-2023 1:00:12 PM

--- ej2-react/code-snippet/chart/series/errorbar-cs7/app/index.jsx
+++ ej2-react/code-snippet/chart/series/errorbar-cs7/app/index.jsx
@@ -5,17 +5,17 @@ import { data } from 'datasource.ts';
 function App() {
     const primaryxAxis = { minimum: 2005, maximum: 2012, interval: 1, title: 'Year' };
     const primaryyAxis = { minimum: 3, maximum: 12, interval: 1, title: 'Percentage', labelFormat: '{value}%' };
-    const marker = {visible: true};
     const errorbar = {
         visible: true, errorBarColorMapping: 'color', verticalError: 'error'
-        }
+    };
     return <ChartComponent id='charts' primaryXAxis={primaryxAxis} primaryYAxis={primaryyAxis} title='Unemployment rate (%)'>
       <Inject services={[LineSeries, Legend, Category, ErrorBar]}/>
       <SeriesCollectionDirective>
-        <SeriesDirective dataSource={data} xName='x' yName='y' width={2} name='India' type='Line' marker={marker} errorBar={errorbar}>
+        <SeriesDirective dataSource={data} xName='x' yName='y' width={2} type='Line' errorBar={errorbar}>
         </SeriesDirective>
       </SeriesCollectionDirective>
     </ChartComponent>;
-};
+}
+;
 export default App;
 ReactDOM.render(<App />, document.getElementById("charts"));
--- ej2-react/code-snippet/chart/series/polar-cs10/app/index.jsx
+++ ej2-react/code-snippet/chart/series/polar-cs10/app/index.jsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, RadarSeries, LineSeries, ColumnSeries } from '@syncfusion/ej2-react-charts';
-import { columnData } from 'datasource.ts';
+import { data } from 'datasource.ts';
 function App() {
     const primaryxAxis = { title: 'Month', startAngle: 90 };
     const primaryyAxis = { minimum: 20, maximum: 40, interval: 5, title: 'Efficiency', labelFormat: '{value}%' };
     return <ChartComponent id='charts' primaryXAxis={primaryxAxis} primaryYAxis={primaryyAxis} title='Efficiency of oil-fired power production'>
       <Inject services={[RadarSeries, LineSeries, ColumnSeries]}/>
       <SeriesCollectionDirective>
-        <SeriesDirective dataSource={columnData} xName='x' yName='y' type='Radar' name='Department' drawType='Column'>
+        <SeriesDirective dataSource={data} xName='x' yName='y' type='Radar' drawType='Line'>
         </SeriesDirective>
       </SeriesCollectionDirective>
     </ChartComponent>;
