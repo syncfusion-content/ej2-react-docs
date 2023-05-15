668984470f42225e1460d9ed1dd40af516315db9
Build Automaion <buildautomation@syncfusion.com>
2023-05-15 17:34:35 +0530

    Integrated latest changes at 05-15-2023 4:02:13 PM

--- ej2-react/code-snippet/file-manager/access-control-cs1/app/App.tsx
+++ ej2-react/code-snippet/file-manager/access-control-cs1/app/App.tsx
@@ -5,21 +5,16 @@ import * as React from 'react';
 function App() {
   let fileObj: FileManagerComponent;
   let hostUrl: string = "https://ej2-aspcore-service.azurewebsites.net/";
-  function onBeforeSend(args) {
-    args.ajaxSettings.beforeSend = function (args) {
-        args.httpRequest.setRequestHeader('Authorization', 'Access control');
-    };
-  }
   return (
   <div>
       <div className="control-section">
           <FileManagerComponent ref={ s => (fileObj = s as FileManagerComponent)} id="file" 
               ajaxSettings = {{
-                downloadUrl: hostUrl +'api/FileManager/Download',
-                getImageUrl: hostUrl +'api/FileManager/GetImage',
-                uploadUrl: hostUrl +'api/FileManager/Upload',
-                url: hostUrl + 'api/FileManager/FileOperations'         
-              }} view={"Details"} beforeSend={onBeforeSend.bind(this)} >
+                downloadUrl: hostUrl +'api/FileManagerAccess/Download',
+                getImageUrl: hostUrl +'api/FileManagerAccess/GetImage',
+                uploadUrl: hostUrl +'api/FileManagerAccess/Upload',
+                url: hostUrl + 'api/FileManagerAccess/FileOperations'         
+              }} view={"Details"} >
               <Inject services={[ NavigationPane, DetailsView, Toolbar]} />
           </FileManagerComponent>
       </div>
