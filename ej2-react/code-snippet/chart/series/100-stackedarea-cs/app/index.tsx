a951625de500771c2d71838c8748c74b484a5002
Build Automaion <buildautomation@syncfusion.com>
2023-05-18 21:42:27 +0530

    Integrated latest changes at 05-18-2023 7:03:07 PM

--- ej2-react/code-snippet/chart/series/100-stackedarea-cs/app/index.tsx
+++ ej2-react/code-snippet/chart/series/100-stackedarea-cs/app/index.tsx
@@ -20,9 +20,9 @@ function App() {
       title='Annual Temperature Comparison'>
       <Inject services={[StackingAreaSeries, Legend, Tooltip, DataLabel, DateTime]} />
       <SeriesCollectionDirective>
-        <SeriesDirective dataSource={percentData} xName='x' yName='y' name='USA' type='StackingArea100'fill='red' border={{width: 2.5, color:'white'}} dashArray='5'>
+        <SeriesDirective dataSource={percentData} xName='x' yName='y' name='USA' type='StackingArea100' fill='red' border={{width: 2.5, color:'white'}} dashArray='5'>
         </SeriesDirective>
-        <SeriesDirective dataSource={percentData} xName='x' yName='y1' name='UK' type='StackingArea100'fill='green' border={{width: 2.5, color:'white'}} dashArray='5'>
+        <SeriesDirective dataSource={percentData} xName='x' yName='y1' name='UK' type='StackingArea100' fill='green' border={{width: 2.5, color:'white'}} dashArray='5'>
         </SeriesDirective>
         <SeriesDirective dataSource={percentData} xName='x' yName='y2' name='Canada Alternatives' type='StackingArea100'fill='orange' border={{width: 2.5, color:'white'}} dashArray='5'>
         </SeriesDirective>
