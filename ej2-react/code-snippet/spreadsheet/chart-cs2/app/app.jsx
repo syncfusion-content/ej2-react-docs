84c29536bb55b233f062c4ea8c7b23832afa0b3a
Build Automaion <buildautomation@syncfusion.com>
2023-06-23 20:23:49 +0530

    Integrated latest changes at 06-23-2023 4:34:36 PM

---
+++ ej2-react/code-snippet/spreadsheet/chart-cs2/app/app.jsx
@@ -0,0 +1,62 @@
+{% raw %}
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { SpreadsheetComponent, SheetsDirective, SheetDirective, RangesDirective, RowsDirective, RowDirective, CellDirective, CellsDirective, CellStyleModel, ChartModel } from '@syncfusion/ej2-react-spreadsheet';
+import { RangeDirective, ColumnsDirective, ColumnDirective, getFormatFromType, Spreadsheet } from '@syncfusion/ej2-react-spreadsheet';
+import { chartData } from './datasource';
+
+function App() {
+    const spreadsheetRef = React.useRef<SpreadsheetComponent>(null);
+    const style = { backgroundColor: '#357cd2', color: '#fff', fontWeight: 'bold', textAlign: 'center', verticalAlign: 'middle' };
+    React.useEffect(() => {
+        let spreadsheet = spreadsheetRef.current;
+        if (spreadsheet) {
+            spreadsheet.cellFormat({ backgroundColor: '#357cd2', color: '#fff', fontWeight: 'bold', textAlign: 'center' }, 'A3:F3');
+            spreadsheet.numberFormat(getFormatFromType('Currency'), 'B4:F8');
+            spreadsheet.merge('A1:F1');
+            //Render Column chart
+            spreadsheet.insertChart([{type: 'Column', theme: 'Bootstrap5Dark', range: 'A3:B6', id: 'column-chart' }]);
+            //Render Line chart with Marker
+            spreadsheet.insertChart([{type: 'Line', range: 'A3:B6', markerSettings: {visible: true, shape: 'Circle', isFilled: false, size: 10, border: {width: 2, color: '#3cb371'}}, id: 'line-chart'}]);
+        }
+    }, []);
+
+    return (
+        <div>
+            <SpreadsheetComponent ref={spreadsheetRef}>
+                <SheetsDirective>
+                    <SheetDirective name='Book Sales'>
+                        <RowsDirective>
+                            <RowDirective height={30}>
+                                <CellsDirective>
+                                    <CellDirective value='Book Sales 2016-2020' style={style}></CellDirective>
+                                </CellsDirective>
+                            </RowDirective>
+                            <RowDirective>
+                                <CellsDirective>
+                                    <CellDirective index={7}></CellDirective>
+                                </CellsDirective>
+                            </RowDirective>
+                        </RowsDirective>
+                        <RangesDirective>
+                            <RangeDirective dataSource={chartData} startCell='A3' ></RangeDirective>
+                        </RangesDirective>
+                        <ColumnsDirective>
+                            <ColumnDirective width={110}></ColumnDirective>
+                            <ColumnDirective width={100}></ColumnDirective>
+                            <ColumnDirective width={100}></ColumnDirective>
+                            <ColumnDirective width={100}></ColumnDirective>
+                            <ColumnDirective width={100}></ColumnDirective>
+                            <ColumnDirective width={100}></ColumnDirective>
+                        </ColumnsDirective>
+                    </SheetDirective>
+                </SheetsDirective>
+            </SpreadsheetComponent>
+        </div>
+    );
+};
+export default App;
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+{% endraw %}
