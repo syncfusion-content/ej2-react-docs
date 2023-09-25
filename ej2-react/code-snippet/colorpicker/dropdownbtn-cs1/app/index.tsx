d765196ceb06cf53b47545ee7825762e558aa65a
Build Automaion <buildautomation@syncfusion.com>
2023-09-25 19:50:14 +0530

    Integrated latest changes at 09-25-2023 7:30:10 PM

--- ej2-react/code-snippet/colorpicker/dropdownbtn-cs1/app/index.tsx
+++ ej2-react/code-snippet/colorpicker/dropdownbtn-cs1/app/index.tsx
@@ -23,12 +23,18 @@ function App() {
         const cancelBtn = parentElem.querySelector('.e-cancel') as HTMLElement;
         cancelBtn.addEventListener('click', closePopup.bind(this));
     }
+
+    function open() {
+        var zindex = (document.getElementsByClassName('e-color-picker-tooltip')[0] as HTMLElement).style.zIndex;
+        var zindexIntValue = parseInt(zindex) + 2;
+        (document.getElementsByClassName('e-color-picker-tooltip')[0] as HTMLElement).style.zIndex = zindexIntValue.toString();
+    }
     return (
         <div id='container'>
         <div className='wrap'>
             <h4>Choose Color</h4>
             <ColorPickerComponent ref={(scope) => { cp = scope as ColorPickerComponent; }} id='colorpicker' inline={true} change={onChange}/>
-            <DropDownButtonComponent id='dropdownbtn' target='.e-colorpicker-wrapper' ref={(scope) => { ddb = scope as DropDownButtonComponent; }} iconCss='e-dropdownbtn-preview' created={onCreated}/>
+            <DropDownButtonComponent id='dropdownbtn' open={open} target='.e-colorpicker-wrapper' ref={(scope) => { ddb = scope as DropDownButtonComponent; }} iconCss='e-dropdownbtn-preview' created={onCreated}/>
         </div>
         </div>
     );
