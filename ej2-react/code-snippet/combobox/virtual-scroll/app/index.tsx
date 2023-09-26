fed2433f5a32b91eade80e1a6dc6192dc801a615
Build Automaion <buildautomation@syncfusion.com>
2023-09-26 21:34:18 +0530

    Integrated latest changes at 09-26-2023 7:30:42 PM

--- ej2-react/code-snippet/combobox/virtual-scroll/app/index.tsx
+++ ej2-react/code-snippet/combobox/virtual-scroll/app/index.tsx
@@ -1,35 +1,27 @@
-
-
-
-// import DataManager related classes
-import { DataManager, ODataV4Adaptor, Query } from '@syncfusion/ej2-data';
-import { SortOrder } from '@syncfusion/ej2-lists';
 import { ComboBoxComponent, Inject, VirtualScroll } from '@syncfusion/ej2-react-dropdowns';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 export default class App extends React.Component<{}, {}> {
     // maps the appropriate column to fields property
-  private fields: object = { text: 'text', value: 'id' };
+    private fields: object = { text: 'text', value: 'id' };
 
-  // define the array of string
-  private records: { [key: string]: Object }[] = [];
-  
-  private updateData(): void {
-   for (let i = 1; i <= 150; i++) {
-       const item: { [key: string]: Object } = {
-           id: 'id' + i,
-           text: `Item ${i}`,
-       };
-       this.records.push(item);
-   }
- }
+    // define the array of string
+    private records: { [key: string]: Object }[] = [];
+
+    constructor(props) {
+        super(props);
+        this.records = Array.from({ length: 150 }, (_, i) => ({
+            id: 'id' + (i + 1),
+            text: `Item ${i + 1}`,
+        }));
+    }
     public render() {
         return (
-             // specifies the tag for render the ComboBox component
-             <ComboBoxComponent id="datas" dataSource={this.updateData()} fields={this.fields} placeholder="e.g. Item 1" enableVirtualization={true} allowFiltering={true} popupHeight="200px" >
-             <Inject services={[VirtualScroll]}/>
-           </ComboBoxComponent>
+            // specifies the tag for render the ComboBox component
+            <ComboBoxComponent id="datas" dataSource={this.records} fields={this.fields} placeholder="e.g. Item 1" enableVirtualization={true} allowFiltering={true} popupHeight="200px" >
+                <Inject services={[VirtualScroll]} />
+            </ComboBoxComponent>
         );
     }
 }
--- ej2-react/code-snippet/dropdownlist/virtual-scroll/app/index.jsx
+++ ej2-react/code-snippet/dropdownlist/virtual-scroll/app/index.jsx
@@ -1,27 +1,23 @@
-// import DataManager related classes
 import { DropDownListComponent, Inject, VirtualScroll } from '@syncfusion/ej2-react-dropdowns';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 export default class App extends React.Component {
     // define the array of string
-    records = [];
-    updateData() {
-        for (var i = 1; i <= 150; i++) {
-            const item = {
-                id: 'id' + i,
-                text: `Item ${i}`,
-            };
-            this.records.push(item);
-        }
+    constructor(props) {
+        super(props);
+        this.records = Array.from({ length: 150 }, (_, i) => ({
+            id: 'id' + (i + 1),
+            text: `Item ${i + 1}`,
+        }));
     }
     fields = { text: 'text', value: 'id' };
 
     render() {
         return (
-        // specifies the tag for render the DropDownList component
-        <DropDownListComponent id="datas" dataSource={this.updateData()} placeholder="e.g. Item 1" enableVirtualization={true} allowFiltering={true} fields={this.fields} popupHeight="200px" >
-      <Inject services={[VirtualScroll]}/>
-  </DropDownListComponent>);
+            // specifies the tag for render the DropDownList component
+            <DropDownListComponent id="datas" dataSource={this.records} placeholder="e.g. Item 1" enableVirtualization={true} allowFiltering={true} fields={this.fields} popupHeight="200px" >
+                <Inject services={[VirtualScroll]} />
+            </DropDownListComponent>);
     }
 }
 ReactDOM.render(<App />, document.getElementById('sample'));
