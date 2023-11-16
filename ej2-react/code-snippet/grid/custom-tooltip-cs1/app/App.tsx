442ec8177ec9791b6868e6a8087826c62de7a8f0
Build Automaion <buildautomation@syncfusion.com>
2023-11-16 16:47:41 +0530

    Integrated latest changes at 11-16-2023 4:30:21 PM

--- ej2-react/code-snippet/grid/custom-tooltip-cs1/app/App.tsx
+++ ej2-react/code-snippet/grid/custom-tooltip-cs1/app/App.tsx
@@ -1,31 +1,25 @@
 
-
-import { getValue } from '@syncfusion/ej2-base';
-import { Column, ColumnDirective, ColumnsDirective, GridComponent } from '@syncfusion/ej2-react-grids';
-import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { ColumnDirective, ColumnsDirective, GridComponent } from '@syncfusion/ej2-react-grids';
 import * as React from 'react';
 import { data } from './datasource';
-
+import { TooltipComponent, TooltipEventArgs } from '@syncfusion/ej2-react-popups';
 function App() {
-  let tooltip: any;
-  let grid: Grid | null;
-  const beforeRender = (args) => {
-    // event triggered before render the tooltip on target element.
-    tooltip.content = args.target.closest("td").innerText;
-  }
-  return <div>
-    <TooltipComponent ref={t => tooltip = t} target='.e-rowcell' beforeRender={beforeRender}>
-      <GridComponent dataSource={data} height={320} ref={g => grid = g}>
-        <ColumnsDirective>
-          <ColumnDirective field='OrderID' headerText='Order ID' width='100' textAlign="Right" />
-          <ColumnDirective field='CustomerID' headerText='Customer ID' width='100' />
-          <ColumnDirective field='EmployeeID' headerText='Employee ID' width='100' textAlign="Right" />
-          <ColumnDirective field='Freight' headerText='Freight' width='100' format="C2" textAlign="Right" />
-          <ColumnDirective field='ShipCountry' headerText='Ship Country' width='100' />
-        </ColumnsDirective>
-      </GridComponent>
-    </TooltipComponent></div>
+  let toolTip: TooltipComponent | null;
+  const beforeRender = ((args: TooltipEventArgs) => {
+    if (args.target.classList.contains('e-rowcell')) {
+      (toolTip as TooltipComponent).content = 'This is value "' + args.target.innerText + '" ';
+    }
+  });
+  return (
+    <div>
+      <TooltipComponent ref={t => toolTip = t} beforeRender={beforeRender} target=".e-rowcell">
+        <GridComponent dataSource={data} height={315}>
+          <ColumnsDirective>
+            <ColumnDirective field='OrderID' headerText='OrderID' width='100' />
+            <ColumnDirective field='CustomerID' headerText='CustomerID' width='120' />
+            <ColumnDirective field='Freight' headerText='Freight' format='C' width='80' />
+            <ColumnDirective field='ShipCity' headerText='ShipCity' width='120' />
+          </ColumnsDirective>
+        </GridComponent></TooltipComponent></div>)
 }
 export default App;
-
-
