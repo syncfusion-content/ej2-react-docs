b970e8a9122a4bbbd4fafe218bd548c23c90658d
Build Automaion <buildautomation@syncfusion.com>
2023-12-26 19:52:01 +0530

    Integrated latest changes at 12-26-2023 7:30:12 PM

---
+++ ej2-react/code-snippet/linear-gauge/pointers-cs10/app/index.tsx
@@ -0,0 +1,108 @@
+{% raw %}
+
+
+
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {
+    LinearGaugeComponent,
+    AxesDirective,
+    AxisDirective,
+    RangesDirective,
+    RangeDirective,
+    PointersDirective,
+    PointerDirective,
+  } from '@syncfusion/ej2-react-lineargauge';
+export function App() {
+   return(
+    <LinearGaugeComponent orientation="Horizontal">
+      <AxesDirective>
+        <AxisDirective
+          line={{ width: 5 }}
+          minorTicks={{
+            interval: 10,
+            height: 3,
+            position: 'Inside',
+          }}
+          majorTicks={{
+            interval: 20,
+            height: 7,
+            width: 1,
+            position: 'Inside',
+          }}
+          labelStyle={{ position: 'Inside', font: { fontFamily: 'inherit' } }}
+          minimum={0}
+          maximum={100}
+          opposedPosition={true}
+        >
+          <PointersDirective>
+            <PointerDirective
+              value={13}
+              markerType="Text"
+              text="Low"
+              color="black"
+              offset={-50}
+              textStyle={{
+                size: '18px',
+                fontWeight: 'bold',
+              }}
+            ></PointerDirective>
+            <PointerDirective
+              value={48}
+              markerType="Text"
+              text="Moderate"
+              color="black"
+              offset={-50}
+              textStyle={{
+                size: '18px',
+                fontWeight: 'bold',
+              }}
+            ></PointerDirective>
+            <PointerDirective
+              value={83}
+              markerType="Text"
+              text="High"
+              color="black"
+              offset={-50}
+              textStyle={{
+                size: '18px',
+                fontWeight: 'bold',
+              }}
+            ></PointerDirective>
+          </PointersDirective>
+          <RangesDirective>
+            <RangeDirective
+              position="Inside"
+              start={0}
+              end={30}
+              color="#FB7D55"
+              startWidth={50}
+              endWidth={50}
+            />
+            <RangeDirective
+              position="Inside"
+              start={30}
+              end={65}
+              color="#ECC85B"
+              startWidth={50}
+              endWidth={50}
+            />
+            <RangeDirective
+              position="Inside"
+              start={65}
+              end={100}
+              color="#6FC78A"
+              startWidth={50}
+              endWidth={50}
+            />
+          </RangesDirective>
+        </AxisDirective>
+      </AxesDirective>
+    </LinearGaugeComponent>);
+}
+const root = ReactDOM.createRoot(document.getElementById('container'));
+root.render(<App />);
+
+
+
+{% endraw %}
---
+++ ej2-react/maps/methods.md
@@ -0,0 +1,30 @@
+---
+layout: post
+title: Methods in React Maps component | Syncfusion
+description: Learn here all about methods in Syncfusion React Maps component of Syncfusion Essential JS 2 and more.
+control: Methods 
+platform: ej2-react
+documentation: ug
+domainurl: ##DomainURL##
+---
+
+# Methods in React Maps component
+
+## Methods
+
+This section explains the methods used in the Maps component.
+
+## getMinMaxLatitudeLongitude
+
+The `getMinMaxLatitudeLongitude` method returns the minimum and maximum latitude and longitude values of the Maps visible area. This method returns a `IMinMaxLatitudeLongitude` object that contains the Maps minimum and maximum latitude and longitude coordinates.
+
+{% tabs %}
+{% highlight js tabtitle="index.jsx" %}
+{% include code-snippet/maps/default-map-cs109/app/index.jsx %}
+{% endhighlight %}
+{% highlight ts tabtitle="index.tsx" %}
+{% include code-snippet/maps/default-map-cs109/app/index.tsx %}
+{% endhighlight %}
+{% endtabs %}
+
+ {% previewsample "page.domainurl/code-snippet/maps/default-map-cs109" %}
