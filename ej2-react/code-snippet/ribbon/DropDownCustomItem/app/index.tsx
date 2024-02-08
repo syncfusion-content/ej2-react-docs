1f034f498754b3116c389e853789a19300a163c6
Build Automaion <buildautomation@syncfusion.com>
2024-02-08 20:02:39 +0530

    Integrated latest changes at 02-08-2024 7:30:13 PM

---
+++ ej2-react/code-snippet/ribbon/DropDownCustomItem/app/index.tsx
@@ -0,0 +1,40 @@
+{% raw %}
+
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { RibbonComponent, RibbonTabsDirective, RibbonTabDirective, RibbonCollectionsDirective, RibbonCollectionDirective, RibbonGroupsDirective, RibbonGroupDirective, RibbonItemsDirective, RibbonItemDirective } from "@syncfusion/ej2-react-ribbon";
+import { ItemModel, MenuEventArgs } from "@syncfusion/ej2-react-splitbuttons";
+
+function App() {
+    const tableOptions: ItemModel[] = [{ text: "Insert Table" }, { text: "This device" }, { text: "Convert Table" }, { text: "Excel SpreadSheet" }];
+    return (
+        <div>
+            <RibbonComponent id="ribbon">
+                <RibbonTabsDirective>
+                    <RibbonTabDirective header="Insert">
+                        <RibbonGroupsDirective>
+                            <RibbonGroupDirective header="Tables">
+                                <RibbonCollectionsDirective>
+                                    <RibbonCollectionDirective>
+                                        <RibbonItemsDirective>
+                                            <RibbonItemDirective type="DropDown" dropDownSettings={{ iconCss: "e-icons e-table", items: tableOptions, content: "Table", beforeItemRender: function (args: MenuEventArgs) { 
+                                                    if (args.item.text === 'Insert Table') {
+                                                        args.element.classList.add("e-custom-class");
+                                                    }
+                                                } }}>
+                                            </RibbonItemDirective>
+                                        </RibbonItemsDirective>
+                                    </RibbonCollectionDirective>
+                                </RibbonCollectionsDirective>
+                            </RibbonGroupDirective>
+                        </RibbonGroupsDirective>
+                    </RibbonTabDirective>
+                </RibbonTabsDirective>
+            </RibbonComponent>
+        </div>
+    );
+}
+export default App;
+ReactDom.render(<App />, document.getElementById("element"));
+
+{% endraw %}
