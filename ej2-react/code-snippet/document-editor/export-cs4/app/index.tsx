b41305d36062168663b3047d585b2e9de0fdd4f4
Build Automaion <buildautomation@syncfusion.com>
2024-03-21 17:16:53 +0530

    Integrated latest changes at 03-21-2024 4:30:31 PM

---
+++ ej2-react/code-snippet/document-editor/export-cs4/app/index.tsx
@@ -0,0 +1,28 @@
+{% raw %}
+
+
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import { DocumentEditorComponent, SfdtExport, Selection, Editor, WordExport } from '@syncfusion/ej2-react-documenteditor';
+
+//Inject require module.
+DocumentEditorComponent.Inject(SfdtExport, Selection, Editor, WordExport);
+function App() {
+    let documenteditor: DocumentEditorComponent;
+    function save() {
+        //Download the document in txt format.
+        documenteditor.save('sample', 'Dotx');
+    }
+        return (
+            <div>
+                <button onClick={save}>Save</button>
+                <DocumentEditorComponent id="container" height={'330px'} ref={(scope) => {documenteditor = scope; }} isReadOnly={false} enableSelection={true} enableEditor={true} enableSfdtExport={true} enableWordExport={true} />
+            </div>
+        );
+}
+export default App;
+ReactDOM.render(<App />, document.getElementById('sample'));
+
+
+
+{% endraw %}
---
+++ ej2-react/code-snippet/ribbon/contextual-tab-method/app/index.tsx
@@ -0,0 +1,73 @@
+{% raw %}
+
+import * as React from "react";
+import * as ReactDom from "react-dom";
+import { RibbonComponent, RibbonTabsDirective, RibbonTabDirective, RibbonCollectionsDirective, RibbonCollectionDirective, RibbonGroupsDirective, RibbonGroupDirective, RibbonItemsDirective, RibbonItemDirective, RibbonContextualTab, RibbonContextualTabsDirective, RibbonContextualTabDirective } from "@syncfusion/ej2-react-ribbon";
+import { ItemModel } from "@syncfusion/ej2-react-splitbuttons";
+import { useRef } from 'react';
+function App() {
+    let ribbonObj = useRef<RibbonComponent>(null);
+    const showContextualTab = () => {
+        ribbonObj.current?.showTab('ArrangeView', true);
+    }
+    const hideContextualTab = () => {
+        ribbonObj.current?.hideTab('ArrangeView', true);
+    }
+    return (
+        <div>
+            <button className="e-btn" id="show-contextual" onClick={showContextualTab}> Show tab </button>
+            <button className="e-btn" id="hide-contextual" onClick={hideContextualTab}> Hide tab </button>
+            <RibbonComponent id='ribbon' ref={ribbonObj}>
+                <RibbonTabsDirective>
+                    <RibbonTabDirective header='Home'>
+                        <RibbonGroupsDirective>
+                            <RibbonGroupDirective header="Clipboard" id="clipboardGroup" groupIconCss="e-icons e-paste" showLauncherIcon={true}>
+                                <RibbonCollectionsDirective>
+                                    <RibbonCollectionDirective>
+                                        <RibbonItemsDirective>
+                                            <RibbonItemDirective type="Button" buttonSettings={{ iconCss: "e-icons e-cut", content: "Cut" }}>
+                                            </RibbonItemDirective>
+                                            <RibbonItemDirective type="Button" buttonSettings={{ iconCss: "e-icons e-copy", content: "Copy" }}>
+                                            </RibbonItemDirective>
+                                            <RibbonItemDirective type="Button" buttonSettings={{ iconCss: "e-icons e-format-painter", content: "Format Painter" }}>
+                                            </RibbonItemDirective>
+                                        </RibbonItemsDirective>
+                                    </RibbonCollectionDirective>
+                                </RibbonCollectionsDirective>
+                            </RibbonGroupDirective>
+                        </RibbonGroupsDirective>
+                    </RibbonTabDirective>
+                </RibbonTabsDirective>
+                <RibbonContextualTabsDirective>
+                    <RibbonContextualTabDirective>
+                        <RibbonTabsDirective>
+                            <RibbonTabDirective id="ArrangeView" header="Arrange & View">
+                                <RibbonGroupsDirective>
+                                    <RibbonGroupDirective header="Arrange" showLauncherIcon={true}>
+                                        <RibbonCollectionsDirective>
+                                            <RibbonCollectionDirective>
+                                                <RibbonItemsDirective>
+                                                    <RibbonItemDirective type="Button" buttonSettings={{ iconCss: "e-icons e-bring-forward", content: "Bring Forward" }}>
+                                                    </RibbonItemDirective>
+                                                    <RibbonItemDirective type="Button" buttonSettings={{ iconCss: "e-icons e-send-backward", content: "Send Backward" }}>
+                                                    </RibbonItemDirective>
+                                                    <RibbonItemDirective type="Button" buttonSettings={{ iconCss: "e-icons e-show-hide-panel", content: "Selection Pane" }}>
+                                                    </RibbonItemDirective>
+                                                </RibbonItemsDirective>
+                                            </RibbonCollectionDirective>
+                                        </RibbonCollectionsDirective>
+                                    </RibbonGroupDirective>
+                                </RibbonGroupsDirective>
+                            </RibbonTabDirective>
+                    </RibbonTabsDirective>
+                    </RibbonContextualTabDirective>
+                </RibbonContextualTabsDirective>
+                <Inject services={[RibbonContextualTab]} />
+            </RibbonComponent>
+        </div>
+    );
+}
+export default App;
+ReactDom.render(<App />, document.getElementById("element"));
+
+{% endraw %}
