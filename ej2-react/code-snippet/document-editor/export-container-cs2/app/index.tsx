9378bae028fcc984ff4b9b685adcbfb6cd777eca
Build Automaion <buildautomation@syncfusion.com>
2024-04-24 02:10:32 +0530

    Integrated latest changes at 04-24-2024 12:36:43 AM

---
+++ ej2-react/code-snippet/document-editor/export-container-cs2/app/index.tsx
@@ -0,0 +1,29 @@
+{% raw %}
+
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
+import { useRef } from 'react';
+import {
+    DocumentEditorContainerComponent, Toolbar
+} from '@syncfusion/ej2-react-documenteditor';
+
+DocumentEditorContainerComponent.Inject(Toolbar);
+
+function App() {
+    // Creating a reference to the DocumentEditorContainerComponent using useRef hook
+    let container = useRef<DocumentEditorContainerComponent>(null); 
+    function save(){
+        //Download the document in Docx format.
+        container.current.documentEditor.save('sample', 'Docx');
+    }
+    return (
+        <div>
+            <button onClick={save}>Save</button>
+            <DocumentEditorContainerComponent id="container" height={'590px'} serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/documenteditor/" enableToolbar={true} ref={container}/>;
+        </div>
+    );
+}
+export default App;
+ReactDOM.render(<App />, document.getElementById('sample'));
+
+{% endraw %}
