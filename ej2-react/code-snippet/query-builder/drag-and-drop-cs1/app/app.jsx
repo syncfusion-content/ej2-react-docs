0bc697dad1ee6a54fffb31c0438ecefa3ffbf138
Build Automaion <buildautomation@syncfusion.com>
2024-06-22 16:42:58 +0530

    Integrated latest changes at 06-22-2024 4:30:12 PM

--- ej2-react/code-snippet/query-builder/drag-and-drop-cs1/app/app.jsx
+++ ej2-react/code-snippet/query-builder/drag-and-drop-cs1/app/app.jsx
@@ -29,14 +29,14 @@ function App() {
         { field: "Country", label: "Country", type: "string" }
     ];
     let importRules = {
-        condition: "",
+        condition: "and",
         rules: [
-            { label: "First Name", field: "FirstName", type: "string", operator: "startswith", value: "Andre", condition: "and" },
-            { label: "Last Name", field: "LastName", type: "string", operator: "in", value: ['Davolio', 'Buchanan'], condition: "or" },
+            { label: "First Name", field: "FirstName", type: "string", operator: "startswith", value: "Andre" },
+            { label: "Last Name", field: "LastName", type: "string", operator: "in", value: ['Davolio', 'Buchanan'] },
             { label: "Age", field: "Age", type: "number", operator: "greaterthan", value: 29, condition: "and" },
             {
                 condition: "or", rules: [
-                    { label: "Is Developer", field: "IsDeveloper", type: "boolean", operator: "equal", value: true, condition: "and" },
+                    { label: "Is Developer", field: "IsDeveloper", type: "boolean", operator: "equal", value: true },
                     { label: "Primary Framework", field: "PrimaryFramework", type: "string", operator: "equal", value: "React" }
                 ]
             },
