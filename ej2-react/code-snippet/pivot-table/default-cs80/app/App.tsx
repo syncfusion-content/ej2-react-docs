92af461de940a2a8c50a7df498d8e28706200c67
Build Automaion <buildautomation@syncfusion.com>
2024-10-08 17:30:45 +0530

    Integrated latest changes at 10-08-2024 4:32:19 PM

--- ej2-react/code-snippet/pivot-table/default-cs80/app/App.tsx
+++ ej2-react/code-snippet/pivot-table/default-cs80/app/App.tsx
@@ -22,10 +22,13 @@ function App() {
   let pivotObj: PivotViewComponent;
   let fieldlistObj: PivotFieldListComponent;
   
-    return (<div><div className="control-section">
-    <PivotViewComponent id='PivotView' ref={ (d: PivotViewComponent) => pivotObj = d } enginePopulated={afterPivotPopulate.bind(this)} height={350} gridSettings={{columnWidth: 140}}></PivotViewComponent>
-    <PivotFieldListComponent id='PivotFieldList' ref={ (d: PivotFieldListComponent) => fieldlistObj = d } enginePopulated={afterPopulate.bind(this)} dataSourceSettings={dataSourceSettings} target='#PivotFieldList' renderMode={"Popup"} allowCalculatedField={true}><Inject services={[CalculatedField]} /></PivotFieldListComponent></div>
-    <div className='col-lg-3 property-section'><ButtonComponent cssClass='e-primary' onClick={btnClick.bind(this)}>Field List</ButtonComponent></div></div>);
+  return (
+    <div>
+      <div className="control-section">
+        <ButtonComponent cssClass='e-primary' onClick={btnClick.bind(this)}>Field List</ButtonComponent>
+        <PivotViewComponent id='PivotView' ref={(d: PivotViewComponent) => pivotObj = d} enginePopulated={afterPivotPopulate.bind(this)} height={350} gridSettings={{ columnWidth: 140 }}></PivotViewComponent>
+        <PivotFieldListComponent id='PivotFieldList' ref={(d: PivotFieldListComponent) => fieldlistObj = d} enginePopulated={afterPopulate.bind(this)} dataSourceSettings={dataSourceSettings} target='#PivotFieldList' renderMode={"Popup"} allowCalculatedField={true}><Inject services={[CalculatedField]} /></PivotFieldListComponent></div>
+      <div className='col-lg-3 property-section'></div></div>);
 
   function btnClick(): void {
     fieldlistObj.dialogRenderer.fieldListDialog.show();
