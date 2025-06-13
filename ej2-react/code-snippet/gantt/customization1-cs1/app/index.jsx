971ff40df8ad18ca731c1ff936b0fe3b408f7517
Build Automaion <buildautomation@syncfusion.com>
2025-06-13 17:40:40 +0530

    Integrated latest changes at 06-13-2025 4:30:13 PM

--- ej2-react/code-snippet/gantt/customization1-cs1/app/index.jsx
+++ ej2-react/code-snippet/gantt/customization1-cs1/app/index.jsx
@@ -5,20 +5,22 @@ import { data } from './datasource';
 
 function App(){
     const taskFields = {
-    id: 'TaskID',
+    id: 'TaskId',
     name: 'TaskName',
     startDate: 'StartDate',
     duration: 'Duration',
     progress: 'Progress',
-    parentID: 'ParentID'
+    parentID: 'ParentId'
   };
   const labelSettings = {
-    leftLabel: 'TaskID',
+    leftLabel: 'TaskId',
     rightLabel: 'Task Name: ${taskData.TaskName}',
     taskLabel: '${Progress}%'
   };
+  const projectStartDate = new Date('03/31/2019');
+  const projectEndDate = new Date('04/18/2019');
         return <GanttComponent dataSource={data} taskFields={taskFields}
-        labelSettings={labelSettings} height = '450px'>
+        labelSettings={labelSettings} height = '450px' projectStartDate={projectStartDate} projectEndDate={projectEndDate}>
         </GanttComponent>
 };
 ReactDOM.render(<App />, document.getElementById('root'));
--- ej2-react/code-snippet/gantt/pdf-export-cs8/app/index.jsx
+++ ej2-react/code-snippet/gantt/pdf-export-cs8/app/index.jsx
@@ -1,34 +1,32 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { ClickEventArgs } from '@syncfusion/ej2-navigations/src/toolbar/toolbar';
 import { GanttComponent, Inject, Toolbar, PdfExport, Selection,Column, ColumnDirective, ColumnsDirective } from '@syncfusion/ej2-react-gantt';
-import { data } from './datasource';
+import { taskData } from './datasource';
 function App() {
-        const taskFields = {
-            id: 'TaskID',
-            name: 'TaskName',
-            startDate: 'StartDate',
-            duration: 'Duration',
-            progress: 'Progress',
-            child: 'subtasks'
-        };
-        const toolbarOptions = ['PdfExport'];
-
-        let ganttChart;
-        function toolbarClick(args) {
-        ganttChart.pdfExport();
+    const taskFields = {
+        id: 'TaskId',
+        name: 'TaskName',
+        startDate: 'StartDate',
+        duration: 'Duration',
+        progress: 'Progress',
+        parentID: 'ParentId',
+    };
+    const toolbarOptions = ['PdfExport'];
+    let ganttObj;
+    function toolbarClick(args) {
+        ganttObj.pdfExport();
     }
 
     function beforePdfExport() {
-        if (ganttChart) {
-            const cols = ganttChart.treeGrid.columns;
+        if (ganttObj) {
+            const cols = ganttObj.treeGrid.columns;
             cols[3].visible = true;
             cols[2].visible = false;
         }
     };
-    return <GanttComponent id='root' dataSource={data} beforePdfExport={beforePdfExport} taskFields={taskFields} toolbar={toolbarOptions} toolbarClick={toolbarClick} allowPdfExport={true} height='400px' ref={gantt => ganttChart = gantt}>
+    return <GanttComponent id='root' dataSource={taskData} beforePdfExport={beforePdfExport} taskFields={taskFields} toolbar={toolbarOptions} toolbarClick={toolbarClick} allowPdfExport={true} height='400px' ref={gantt => ganttObj = gantt}>
         <ColumnsDirective>
-                    <ColumnDirective field='TaskID' width='50'/>
+                    <ColumnDirective field='TaskId' width='100'/>
                     <ColumnDirective field='TaskName' headerText='Task Name'/>
                     <ColumnDirective field='StartDate'/>
                     <ColumnDirective field='Duration' visible={false}/>
