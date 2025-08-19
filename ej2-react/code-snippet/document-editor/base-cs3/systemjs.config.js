4758c58040ce73cf03c25fecea7c434f8b5a9b6a
Build Automaion <buildautomation@syncfusion.com>
2025-08-19 13:33:48 +0530

    Integrated latest changes at 08-19-2025 12:23:23 PM

--- ej2-react/code-snippet/document-editor/base-cs3/systemjs.config.js
+++ ej2-react/code-snippet/document-editor/base-cs3/systemjs.config.js
@@ -46,6 +46,7 @@ System.config({
         "@syncfusion/ej2-react-documenteditor": "syncfusion:ej2-react-documenteditor/dist/ej2-react-documenteditor.umd.min.js",        
         "react-dom": "https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js",
         "react": "https://unpkg.com/react@18.2.0/umd/react.production.min.js",
+        "react-dom/client": "https://unpkg.com/react-dom@18.2.0/umd/react-dom.production.min.js",
     },
     packages: {
         'app': { main: 'index', defaultExtension: 'tsx' },
