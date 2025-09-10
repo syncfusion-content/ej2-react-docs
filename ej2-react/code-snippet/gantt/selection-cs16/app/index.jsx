414875c3e6061dc5d64d99de8dd518e66eed787d
Build Automaion <buildautomation@syncfusion.com>
2025-09-10 20:46:21 +0530

    Integrated latest changes at 09-10-2025 7:30:19 PM

--- ej2-react/code-snippet/gantt/selection-cs16/app/index.jsx
+++ ej2-react/code-snippet/gantt/selection-cs16/app/index.jsx
@@ -1,24 +1,27 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { GanttComponent, Inject, Selection} from '@syncfusion/ej2-react-gantt';
+import { GanttComponent, Inject, Selection } from '@syncfusion/ej2-react-gantt';
 import { data } from './datasource';
-function App (){
-    const  taskFields = {
-    id: 'TaskID',
-    name: 'TaskName',
-    startDate: 'StartDate',
-    duration: 'Duration',
-    progress: 'Progress',
-    parentID: 'ParentID'
-  };
- const settings = {
-        mode: 'Row',
-        type: 'Multiple'
-  };
-  let ganttInstance ;
-        return <GanttComponent dataSource={data} allowSelection={true} enableHover={true} taskFields={taskFields}
-        selectionSettings={settings} height = '450px' ref={gantt =>ganttInstance = gantt}>
-        <Inject services={[Selection]} />
-        </GanttComponent>
+
+function App() {
+      let ganttInstance;
+      const taskFields = {
+            id: 'TaskID',
+            name: 'TaskName',
+            startDate: 'StartDate',
+            duration: 'Duration',
+            progress: 'Progress',
+            parentID: 'ParentID'
+      };
+      const settings = {
+            mode: 'Row',
+            type: 'Multiple'
+      };
+     
+      return <GanttComponent dataSource={data} allowSelection={true} enableHover={true} taskFields={taskFields}
+            selectionSettings={settings} height='450px' ref={gantt => ganttInstance = gantt}>
+            <Inject services={[Selection]} />
+      </GanttComponent>
 };
+
 ReactDOM.render(<App />, document.getElementById('root'));
