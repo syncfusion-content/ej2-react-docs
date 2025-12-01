2d63666328d73b887a30202ad6bf4c362d3c9f50
Build Automaion <buildautomation@syncfusion.com>
2025-12-01 16:43:14 +0530

    Integrated latest changes at 12-01-2025 4:30:04 PM

--- ej2-react/code-snippet/kanban/kanban-to-schedule-cs4/app/index.tsx
+++ ej2-react/code-snippet/kanban/kanban-to-schedule-cs4/app/index.tsx
@@ -1,4 +1,3 @@
-{% raw %}
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { extend, closest, removeClass } from '@syncfusion/ej2-base';
@@ -104,4 +103,3 @@ function App(){
     );
 }
 ReactDOM.render(<App />, document.getElementById('kanban'));
-{% endraw %}
