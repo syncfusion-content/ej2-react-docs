c53e5767aa56940d5779eaf93777a18684b9e203
Build Automaion <buildautomation@syncfusion.com>
2025-12-05 19:42:43 +0530

    Integrated latest changes at 12-05-2025 7:30:03 PM

--- ej2-react/code-snippet/grid/selection-cs10/app/App.tsx
+++ ej2-react/code-snippet/grid/selection-cs10/app/App.tsx
@@ -4,11 +4,11 @@ import { data } from './datasource';
 
 function App() {
   const selectionSettings: SelectionSettingsModel = { mode: 'Both', allowColumnSelection: true, type: 'Multiple', persistSelection: true };
-  const pageOptions: PageSettingsModel = { pageSize: 5 };
+  const pageOptions: PageSettingsModel = { pageSize: 8 };
   return (<div>
     <GridComponent dataSource={data} selectionSettings={selectionSettings} allowPaging={true} pageSettings={pageOptions}>
       <ColumnsDirective>
-        <ColumnDirective field='OrderID' headerText='Order ID' width='120' textAlign="Right" />
+        <ColumnDirective field='OrderID' headerText='Order ID' width='120' textAlign="Right" isPrimaryKey={true} />
         <ColumnDirective field='CustomerID' headerText='Customer ID' width='150' />
         <ColumnDirective field='ShipCity' headerText='Ship City' width='100' />
         <ColumnDirective field='ShipName' headerText='Ship Name' width='150' />
