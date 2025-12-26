20fbde600ed3ed6e4c08e0a659ddf626807ee0de
Build Automaion <buildautomation@syncfusion.com>
2025-12-26 19:59:44 +0530

    Integrated latest changes at 12-26-2025 7:30:29 PM

--- ej2-react/code-snippet/pivot-table/default-cs65/app/App.tsx
+++ ej2-react/code-snippet/pivot-table/default-cs65/app/App.tsx
@@ -1,5 +1,5 @@
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
-import { IDataSet, PivotViewComponent } from '@syncfusion/ej2-react-pivotview';
+import { IDataSet, PivotViewComponent, ExcelExport, Inject } from '@syncfusion/ej2-react-pivotview';
 import { DataSourceSettingsModel } from '@syncfusion/ej2-pivotview/src/model/datasourcesettings-model';
 import * as React from 'react';
 import { pivotData } from './datasource';
@@ -16,8 +16,8 @@ function App() {
     values: [{ name: 'Sold', caption: 'Units Sold' }, { name: 'Amount', caption: 'Sold Amount' }]
   }
   let pivotObj: PivotViewComponent;
-  
-  return (<div><div className="col-md-9"> <PivotViewComponent  ref={ (d: PivotViewComponent) => pivotObj = d } id='PivotView' height={350} dataSourceSettings={dataSourceSettings} allowExcelExport={true}></PivotViewComponent></div>
+
+  return (<div><div className="col-md-9"> <PivotViewComponent ref={(d: PivotViewComponent) => pivotObj = d} id='PivotView' height={350} dataSourceSettings={dataSourceSettings} allowExcelExport={true}><Inject services={[ExcelExport]} /></PivotViewComponent></div>
     <div className='col-lg-3 property-section'><ButtonComponent cssClass='e-primary' onClick={btnClick.bind(this)}>Export</ButtonComponent></div></div>);
 
   function btnClick(): void {
--- ej2-react/code-snippet/pivot-table/default-cs68/app/App.tsx
+++ ej2-react/code-snippet/pivot-table/default-cs68/app/App.tsx
@@ -1,5 +1,5 @@
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
-import { IDataSet, PivotViewComponent } from '@syncfusion/ej2-react-pivotview';
+import { IDataSet, PivotViewComponent, ExcelExport, Inject } from '@syncfusion/ej2-react-pivotview';
 import { DataSourceSettingsModel } from '@syncfusion/ej2-pivotview/src/model/datasourcesettings-model';
 import * as React from 'react';
 import { pivotData } from './datasource';
@@ -17,20 +17,20 @@ function App() {
     values: [{ name: 'Sold', caption: 'Units Sold' }, { name: 'Amount', caption: 'Sold Amount' }]
   }
   let pivotObj: PivotViewComponent;
-  
-  return (<div><div className="col-md-9"> <PivotViewComponent  ref={ (d: PivotViewComponent) => pivotObj = d } id='PivotView' height={350} dataSourceSettings={dataSourceSettings} allowExcelExport={true}></PivotViewComponent></div>
+
+  return (<div><div className="col-md-9"> <PivotViewComponent ref={(d: PivotViewComponent) => pivotObj = d} id='PivotView' height={350} dataSourceSettings={dataSourceSettings} allowExcelExport={true}><Inject services={[ExcelExport]} /></PivotViewComponent></div>
     <div className='col-lg-3 property-section'><ButtonComponent cssClass='e-primary' onClick={btnClick.bind(this)}>Export</ButtonComponent></div></div>);
 
   function btnClick(): void {
     let excelExportProperties: ExcelExportProperties = {
       theme:
       {
-          header: { fontName: 'Segoe UI', fontColor: '#666666' },
-          record: { fontName: 'Segoe UI', fontColor: '#666666' },
-          caption: { fontName: 'Segoe UI', fontColor: '#666666' }
+        header: { fontName: 'Segoe UI', fontColor: '#666666' },
+        record: { fontName: 'Segoe UI', fontColor: '#666666' },
+        caption: { fontName: 'Segoe UI', fontColor: '#666666' }
       }
     };
-    pivotObj.grid.excelExport(excelExportProperties);
+    pivotObj.excelExport(excelExportProperties);
   }
 };
 
