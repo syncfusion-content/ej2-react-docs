6161bb19cb69363ddd7746f627747f36d8be4e2f
Build Automaion <buildautomation@syncfusion.com>
2026-04-10 21:10:19 +0530

    Integrated latest changes at 04-10-2026 8:36:29 PM

--- ej2-react/code-snippet/dialog/dialog-utility-alert-cs2/app/App.jsx
+++
@@ -1,9 +0,0 @@
-import { DialogUtility } from '@syncfusion/ej2-popups';
-import * as React from "react";
-function App() {
-    function buttonClick() {
-        DialogUtility.alert('This is an Alert Dialog!');
-    }
-    return (<button className="e-control e-btn" onClick={buttonClick} id="targetButton" role="button">Open Alert Dialog</button>);
-}
-export default App;
--- ej2-react/code-snippet/dialog/dialog-utility-confirm-cs2/app/App.jsx
+++
@@ -1,9 +0,0 @@
-import { DialogUtility } from '@syncfusion/ej2-popups';
-import * as React from "react";
-function App() {
-    function buttonClick() {
-        DialogUtility.confirm('This is a Confirmation Dialog!');
-    }
-    return (<button className="e-control e-btn" onClick={buttonClick.bind(this)} id="targetButton" role="button">Open Confirm Dialog</button>);
-}
-export default App;
