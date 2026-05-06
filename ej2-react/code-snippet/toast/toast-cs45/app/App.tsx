d8805ce1c00041086cf2dcb6295b29f11e00a69f
Build Automaion <buildautomation@syncfusion.com>
2026-05-06 20:11:59 +0530

    Integrated latest changes at 05-06-2026 7:31:20 PM

--- ej2-react/code-snippet/toast/toast-cs45/app/App.tsx
+++ ej2-react/code-snippet/toast/toast-cs45/app/App.tsx
@@ -3,7 +3,7 @@
 import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
 import { ToastComponent, ToastUtility } from '@syncfusion/ej2-react-notifications';
 import * as React from "react";
-const toastObj;
+let toastObj;
 class App extends React.Component<{}, {}> {
   public infoShow() {
     toastObj = ToastUtility.show('Please read the comments carefully', 'Information', 20000);
