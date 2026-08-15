5d4f338e3a0d3060574f7f188e8854820803617f
Build Automaion <buildautomation@syncfusion.com>
2026-08-15 19:39:10 +0530

    Integrated latest changes at 08-15-2026 7:30:03 PM

--- ej2-react/code-snippet/stepper/events/created/systemjs.config.js
+++ ej2-react/code-snippet/stepper/events/created/systemjs.config.js
@@ -28,6 +28,7 @@ System.config({
         "@syncfusion/ej2-data": "syncfusion:ej2-data/dist/ej2-data.umd.min.js",
         "@syncfusion/ej2-inputs": "syncfusion:ej2-inputs/dist/ej2-inputs.umd.min.js",
         "@syncfusion/ej2-splitbuttons": "syncfusion:ej2-splitbuttons/dist/ej2-splitbuttons.umd.min.js",
+        "@syncfusion/ej2-notifications":"syncfusion:ej2-notifications/dist/ej2-notifications.umd.min.js",
         "@syncfusion/ej2-react-base": "syncfusion:ej2-react-base/dist/ej2-react-base.umd.min.js",
         "@syncfusion/ej2-react-popups": "syncfusion:ej2-react-popups/dist/ej2-react-popups.umd.min.js",
         "@syncfusion/ej2-react-navigations": "syncfusion:ej2-react-navigations/dist/ej2-react-navigations.umd.min.js",
